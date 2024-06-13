b479702dd879801065209177341327712cce6bd0
Artur Prentki <76473377+ArturPrentki@users.noreply.github.com>
2024-06-13 17:27:07 +0200

    adding MMAC

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -4,12 +4,27 @@ from .controller import Controller
 
 
 class FeedbackLinearizationController(Controller):
-    def __init__(self, Tp):
-        self.model = ManiuplatorModel(Tp)
+    def __init__(self, Tp, m3=0, r3=0.05):
+        self.model = ManiuplatorModel(Tp, m3, r3)
+        self.Kd = np.array([10.0, 10.0]).reshape(1, 2)
+        self.Kp = np.array([25.0, 25.0]).reshape(1, 2)
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         """
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+        q = np.transpose(np.array([x[:2]]))
+        q_dot = np.transpose(np.array([x[2:]]))
+        if q_r_ddot.shape != (2, 1):
+            v = q_r_ddot[:, np.newaxis]
+        else:
+            v = q_r_ddot
+        # Adding feedback
+        v -= self.Kd@(q_dot - q_r_dot[:, np.newaxis]) + \
+            self.Kp @ (q - q_r[:, np.newaxis])
+
+        M = self.model.M(x)
+        C = self.model.C(x)
+        tau = M @ v + C @ q_dot
+        return tau
